099e1fd379956005661a0f7b68a71c08f7ead444
kkaran2472006-dotcom <kkaran2472006@gmail.com>
2026-02-23 13:39:32 +0200

    Created database connection and fetched patterns data

--- crossbow.py
+++ crossbow.py
@@ -1,8 +1,37 @@
+import mariadb
+import random
+import os
+from dotenv import load_dotenv
 
-pattern = [1,2,3,4]
+load_dotenv()
 
-def printing_pattern():
-    for each in pattern:
-        print(each)
+def database_connection():
+ 
+    config = {
+        "host": os.getenv("DB_HOST"),
+        "port": int(os.getenv("DB_PORT", 3306)), # Cast to int; 3306 is the fallback
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        "database": os.getenv("DB_NAME")
 
-printing_pattern()
+ }
+
+    try:
+        conn = mariadb.connect(**config)
+        print("Server Loaded!\n")
+
+        cur = conn.cursor()
+
+        query = "SELECT * FROM Patterns ;"
+        cur.execute(query)
+
+        fetched_data = cur.fetchall()
+
+        return fetched_data
+
+    except mariadb.Error as e:
+        print(f"Error in MariaDB: {e}")
+        return []
+
+fetched_pattern = database_connection()
+print(fetched_pattern)
